3c6844e70fd2009c58314775a68af8cceca615e8
UDAY VARA <udayvara26@gmail.com>
2023-05-31 16:31:21 +0530

    Level 4 - Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-// var encrypt = require('mongoose-encryption');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -25,8 +26,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
-
 const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
@@ -44,37 +43,46 @@ app.get('/login', (req, res) => {
 
 app.post('/register', (req, res) => {
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
+    bcrypt.hash(req.body.username, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
 
-    newUser.save()
-        .then(() => {
-            res.render("secrets");
-        }).catch((err) => {
-            console.error(err);
-        })
+        newUser.save()
+            .then(() => {
+                res.render("secrets");
+            }).catch((err) => {
+                console.error(err);
+            });
+    });
 });
 
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username })
         .then((foundUser) => {
-            if (foundUser && foundUser.password === password) {
-                res.render("secrets");
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result == true) {
+                        res.render("secrets");
+                    } else {
+                        res.send("Invalid username or password.");
+                    }
+                });
             } else {
                 res.send("Invalid username or password.");
             }
         })
         .catch((err) => {
             console.error(err);
-            res.send("An error occurred here something are missing Please try again");
+            res.send("An error occurred. Please try again.");
         });
 });
+
 
 
 app.listen(3000, function () {
